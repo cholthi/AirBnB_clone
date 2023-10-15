62d2206881fc2949a4ec34ffc4de5670cc46966b
Samuel Amihere <amisam2000@gmail.com>
2023-10-15 13:19:25 +0800

    update console

--- console.py
+++ console.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 """The main console application AirBnB clone"""
 import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Console command application interpreter"""
+    prompt = "(hbnb) "
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
