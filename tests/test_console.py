9bf5fa623faeb7637049994b231f786179b09ec0
Samuel Amihere <amisam2000@gmail.com>
2023-10-17 22:04:31 +0800

    add console test

---
+++ tests/test_console.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Unit tests for BaseModels"""
+from datetime import datetime
+import os
+import unittest
+
+
+class TestConsole(unittest.TestCase):
+    """Unittests for testing instantiation of the
+    HBNBCommand class.
+    """
+    pass
+
