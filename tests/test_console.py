c6efa5f8df135ff5294c800e125901bb4fcfcb06
Samuel Amihere <amisam2000@gmail.com>
2023-10-22 00:54:55 +0800

    console test

--- tests/test_console.py
+++ tests/test_console.py
@@ -8,6 +8,10 @@ from unittest.mock import patch
 from io import StringIO
 from console import HBNBCommand
 from models.engine.file_storage import FileStorage
+from models import storage
+
+available_classes = classes = {'User', 'BaseModel', 'City',
+                               'Review', 'Place', 'State', 'Amenity'}
 
 
 class TestHBNBCmd_prompting(unittest.TestCase):
@@ -116,10 +120,36 @@ class TestHBNBCmd_help(unittest.TestCase):
 
 class TestHBNBCmd_create(unittest.TestCase):
     """Unittest for testing create command and its args"""
-    def test_do_create_object(self):
-        """Tests do_create an object"""
-        pass
+    @classmethod
+    def setUpClass(cls):
+        """Executed before any of the tests are run"""
+        try:
+            os.rename("data.json", "temp")
+        except IOError:
+            pass
+        FileStorage._FileStorage__objects = {}
 
+    @classmethod
+    def tearDownClass(cls):
+        """Executed after all the tests have run"""
+        try:
+            os.remove("data.json")
+        except IOError:
+            pass
+        try:
+            os.rename("temp", "data.json")
+        except IOError:
+            pass
+
+    def test_do_create_object(self):
+        """Tests for create method creating an object"""
+        for clss in available_classes:
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd(
+                    "create {}".format(clss)))
+                self.assertLess(0, len(output.getvalue().strip()))
+                testKey = "{}.{}".format(clss, output.getvalue().strip())
+                self.assertIn(testKey, storage.all().keys())
 
     def test_do_create_missing_class(self):
         """Tests do_create where class is missing"""
@@ -147,15 +177,162 @@ class TestHBNBCmd_create(unittest.TestCase):
 
 
 class TestHBNBCmd_update(unittest.TestCase):
-    def test_do_update_missing_arguments(self):
-        console = HBNBCommand()
-        console.onecmd("update")
-        self.assertEqual("(hbnb) ", console.prompt)
+    """Unittest for testing update command and its args"""
+    @classmethod
+    def setUpClass(cls):
+        """Executed before any of the tests are run"""
+        try:
+            os.rename("data.json", "temp")
+        except IOError:
+            pass
+        FileStorage._FileStorage__objects = {}
 
-    def test_do_update_invalid_class(self):
-        console = HBNBCommand()
-        console.onecmd("update InvalidClass 1234-1234-1234")
-        self.assertEqual("(hbnb) ", console.prompt)
+    @classmethod
+    def tearDownClass(cls):
+        """Executed after all the tests have run"""
+        try:
+            os.remove("data.json")
+        except IOError:
+            pass
+        try:
+            os.rename("temp", "data.json")
+        except IOError:
+            pass
+
+    def test_do_update_missing_arguments(self):
+        """Tests do_update where class is missing"""
+        st = f"** class name missing **"
+        with patch("sys.stdout", new=StringIO()) as output:
+            console = HBNBCommand()
+            self.assertFalse(console.onecmd("create"))
+            self.assertTrue(st, output.getvalue().strip())
+
+    def test_update_missing_id(self):
+        """Tests update for unavailable id
+        """
+        st = f"** instance id missing **"
+        for clss in available_classes:
+            # update lacks id in argument case
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd("update {}".
+                                                      format(clss)))
+                self.assertEqual(st, output.getvalue().strip())
+            # update lacks id for dot notation case
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd("{}.update()".
+                                                      format(clss)))
+                self.assertEqual(st, output.getvalue().strip())
+
+    def test_update_invalid_id(self):
+        """Tests update for unavailable id
+        """
+        st = f"** no instance found **"
+        for clss in available_classes:
+            # update lacks id in argument case
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd("update {} 3".
+                                                      format(clss)))
+                self.assertEqual(st, output.getvalue().strip())
+            # update lacks id for dot notation case
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd("{}.update(3)".
+                                                      format(clss)))
+                self.assertEqual(st, output.getvalue().strip())
+
+    def test_update_missing_attr_name(self):
+        """Tests update for missing attribute name
+        """
+        st = f"** attribute name missing **"
+        for clss in available_classes:
+            # update lacks id in argument case
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd("create {}".
+                                                      format(clss)))
+                testId = output.getvalue().strip()
+                testCmd = "update {} {}".format(clss, testId)
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd(testCmd))
+                self.assertEqual(st, output.getvalue().strip())
+            # update lacks id for dot notation case
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd("create {}".
+                                                      format(clss)))
+                testId = output.getvalue().strip()
+                testCmd = "{}.update({})".format(clss, testId)
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd(testCmd))
+                self.assertEqual(st, output.getvalue().strip())
+
+    def test_update_missing_attr_value_space_notation(self):
+        """Tests update for missing attribute value"""
+        st = f"** value missing **"
+        for clss in available_classes:
+            # update lacks id in argument case
+            with patch("sys.stdout", new=StringIO()) as output:
+                HBNBCommand().onecmd("create {}".format(clss))
+                testId = output.getvalue().strip()
+            with patch("sys.stdout", new=StringIO()) as output:
+                testCmd = "update {} {} attr_name".format(clss, testId)
+                self.assertFalse(HBNBCommand().onecmd(testCmd))
+                self.assertEqual(st, output.getvalue().strip())
+            # update lacks id for dot notation case
+            with patch("sys.stdout", new=StringIO()) as output:
+                HBNBCommand().onecmd("create {}".format(clss))
+                testId = output.getvalue().strip()
+            with patch("sys.stdout", new=StringIO()) as output:
+                testCmd = "{}.update({}, attr_name)".format(clss, testId)
+                self.assertFalse(HBNBCommand().onecmd(testCmd))
+                self.assertEqual(st, output.getvalue().strip())
+
+    def test_update_valid_int(self):
+        """Tests update for valid int"""
+        # update lacks id in argument case
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd("create Place")
+            testId = output.getvalue().strip()
+        testCmd = "update Place {} max_guest 20".format(testId)
+        self.assertFalse(HBNBCommand().onecmd(testCmd))
+        test_dict = storage.all()["Place.{}".format(testId)].__dict__
+        self.assertEqual(20, test_dict["max_guest"])
+
+        # update lacks id for dot notation case
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd("create Place")
+            tId = output.getvalue().strip()
+        testCmd = "Place.update({}, max_guest, 20)".format(tId)
+        self.assertFalse(HBNBCommand().onecmd(testCmd))
+        test_dict = storage.all()["Place.{}".format(tId)].__dict__
+        self.assertEqual(20, test_dict["max_guest"])
+
+    def test_update_valid_string_attr(self):
+        """Tests update for valid string"""
+        for clss in available_classes:
+            # update lacks id in argument case
+            with patch("sys.stdout", new=StringIO()) as output:
+                HBNBCommand().onecmd("create {}".format(clss))
+                testId = output.getvalue().strip()
+            testCmd = "update {} {} attr_name 'attr_value'".format(
+                clss, testId)
+            self.assertFalse(HBNBCommand().onecmd(testCmd))
+            test_dict = storage.all()["{}.{}".format(clss, testId)].__dict__
+            self.assertEqual("attr_value", test_dict["attr_name"])
+            # update lacks id for dot notation case
+            with patch("sys.stdout", new=StringIO()) as output:
+                HBNBCommand().onecmd("create {}".format(clss))
+                tId = output.getvalue().strip()
+            testCmd = "{}.update({}, attr_name, 'attr_value')".format(
+                clss, tId)
+            self.assertFalse(HBNBCommand().onecmd(testCmd))
+            test_dict = storage.all()["{}.{}".format(clss, tId)].__dict__
+            self.assertEqual("attr_value", test_dict["attr_name"])
+
+    def test_do_create_invalid_syntax(self):
+        """Tests do_update where syntax is invalid"""
+        st = f"*** Unknown syntax: .create."
+        with patch("sys.stdout", new=StringIO()) as output:
+            console = HBNBCommand()
+            self.assertFalse(console.onecmd(".create. "))
+            self.assertEqual(st, output.getvalue().strip())
 
 
 class TestHBNBCmd_show(unittest.TestCase):
@@ -191,3 +368,4 @@ class TestHBNBCmd_destroy(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
